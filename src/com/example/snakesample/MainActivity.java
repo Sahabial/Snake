3e086090c4255990cbde26f5f8267210eaf5aec2
sahabial <sahabial@tut.by>
2013-09-10 21:44:40 +0300

    tests

--- src/com/example/snakesample/MainActivity.java
+++ src/com/example/snakesample/MainActivity.java
@@ -1,23 +1,43 @@
 package com.example.snakesample;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
+	
+	 String tests[] = { "LifeCycleTest", "SingleTouchTest", "MultiTouchTest",
+	            "KeyTest", "AccelerometerTest", "AssetsTest",
+	            "ExternalStorageTest", "SoundPoolTest", "MediaPlayerTest",
+	            "FullScreenTest", "RenderViewTest", "ShapeTest", "BitmapTest",
+	            "FontTest", "SurfaceViewTest" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    	 super.onCreate(savedInstanceState);
+         setListAdapter(new ArrayAdapter<String>(this,
+                 android.R.layout.simple_list_item_1, tests));
     }
-
-
+    
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    protected void onListItemClick(ListView list, View view, int position,
+            long id) {
+        super.onListItemClick(list, view, position, id);
+        String testName = tests[position];
+        try {
+            Class clazz = Class
+                    .forName("framework." + testName);
+            Intent intent = new Intent(this, clazz);
+            startActivity(intent);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+
+
+    
     
 }
---
+++ src/framework/SingleTouchTest.java
@@ -0,0 +1,63 @@
+package framework;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.TextView;
+
+public class SingleTouchTest extends Activity implements OnTouchListener{
+
+		StringBuilder builder = new StringBuilder();
+		TextView textView;
+		
+
+		public void onCreate(Bundle savedInstanceState) {
+			// TODO Auto-generated method stub
+			super.onCreate(savedInstanceState);
+			textView = new TextView(this);
+			textView.setText("Touch and drug via One finger only");
+			textView.setOnTouchListener(this);
+			setContentView(textView);
+		}
+		
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			// TODO Auto-generated method stub
+			
+			builder.setLength(0);
+			switch (event.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				builder.append("down. ");
+				break;
+			case MotionEvent.ACTION_MOVE:
+				builder.append("move. ");
+				break;
+			case MotionEvent.ACTION_CANCEL:
+				builder.append("cancel. ");
+				break;
+			case MotionEvent.ACTION_UP:
+				builder.append("up. ");
+				break;
+			}
+			builder.append(event.getX());
+			builder.append(". ");
+			builder.append(event.getY());
+			String text = builder.toString();
+			Log.d("TouchTest", text);
+			textView.setText(text);
+			
+			try {
+				Thread.sleep(16);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				
+			}
+			
+			return true;
+		}
+		
+		
+}
